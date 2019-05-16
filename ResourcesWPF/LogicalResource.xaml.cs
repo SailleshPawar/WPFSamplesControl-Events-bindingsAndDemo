b77c80c15849ccbf42ad053e2c9cec1e800703ff
Saillesh Pawar <psaillesh@gmail.com>
2019-05-16 16:21:30 +0530

    Template

---
+++ ResourcesWPF/LogicalResource.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ResourcesWPF
+{
+    /// <summary>
+    /// Interaction logic for LogicalResource.xaml
+    /// </summary>
+    public partial class LogicalResource : Window
+    {
+        public LogicalResource()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnClick_Click(object sender, RoutedEventArgs e)
+        {
+            this.Resources["CustomBrush"] = new SolidColorBrush(Colors.Green);
+        }
+    }
+}
